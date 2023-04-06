e6d20dea70deffce2f323271c78005524da51a90
“[BISHAL <[bishalaryan12300@gmail.com]>
2023-04-06 13:36:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// Scroll section active link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+
+
+    // Stick nav bar 
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+//Scroll Reversal
+
+ScrollReveal({ 
+    
+    // reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container , .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-img , about-content', { origin: 'right' });
+
+//Typed JS
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Developer', 'Designer', 'Full Stack'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay:100,
+    loop: true
+
+});
